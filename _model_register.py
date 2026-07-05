615427db67bb5ab0b4a81eed8aedacbaea2dde36
Senqi-Guan <145045447+Senqi-Guan@users.noreply.github.com>
2026-07-05 21:24:54 +0800

    Update _model_register.py

--- _model_register.py
+++ _model_register.py
@@ -54,17 +54,5 @@ _MODEL_POOL = {
         verbose=-1,
         feature_fraction=1.0,
         bagging_fraction=1.0
-    ),
-    'Metalearning Model': StackingClassifier(
-        estimators=[
-            ('lr', LogisticRegression(random_state=42, max_iter=500, C=0.3, class_weight='balanced')),
-            ('lightgbm', LGBMClassifier(n_estimators=200, learning_rate=0.05, max_depth=4, num_leaves=4, random_state=42, class_weight='balanced', verbose=-1, feature_fraction=1, bagging_fraction=1)),
-            ('SVM', SVC(kernel='rbf', probability=True, random_state=42, C=0.8, gamma='scale', class_weight='balanced')),
-        ],
-        final_estimator=LogisticRegression(random_state=42, max_iter=2000, C=0.5, class_weight='balanced'),
-        cv=5,
-        stack_method='predict_proba',
-        n_jobs=-1,
-        passthrough=True,
     )
 }
